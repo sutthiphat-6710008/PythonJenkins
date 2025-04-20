5465e02f9ba00fff826a79f9c6ee1696d6dfd12b
sutthiphat <S6752D10008@sau.ac.th>
2025-04-20 10:29:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print('Hello World')
 print('Python programming is very easy')
 print('How are you?')
-print('I am fine, thank you and you?
+print('I am fine, thank you and you?')
 print('How do you do?')
+print('I am developer')
